d50ef20c0db57956b9c583bd4ebefc2d2ec76b5b
jenny <lyndle0890@gmail.com>
2017-08-31 18:19:03 -0700

    --> code refactoring --> responsive adjustments

--- picGallery/JS/custom/main.js
+++ picGallery/JS/custom/main.js
@@ -9,6 +9,33 @@
 	var maxYear = 0;
 	var minYear = 0;
 	
+	var load = function(elem,state){
+		if(state)
+			{
+				
+				$("#loader").css({
+					  opacity : 0.5,
+					  //top     : elem.offset().top,
+					  width   : elem.outerWidth(),
+					  height  : elem.outerHeight()
+					});
+				console.log(elem.attr('class')+' '+'height:'+elem.height());
+				console.log(elem.attr('class')+' '+'width:'+elem.width());
+
+					$("#img-load").css({
+					  top  : ($("#loader").height() / 2),
+					  //left : ($("#loader").width() / 2)
+					});
+					$("#loader").show();
+
+			}
+		else
+			{
+			$("#loader").fadeOut();
+			}
+	}
+
+	
 	var maintainYearSelection = function(){
 		$('.year').removeClass('active');
 		var elem = $('.yearContent:contains('+selectedYear+')');
@@ -28,26 +55,19 @@
 		$('.content img').attr('src',imgSrc).data('currImgIndex',index);
 		$('.picHeading').text(imgHeading);
 		$('.picDesc').text(imgDescription);
-		$('.overlay, .content').show();
+		$('.overlay, .content').fadeIn();
 	};
 	
 	var createTimeLine = function(data){
-		//var timeLineContainer = $('<div class="container-fluid"></div>');
 		var timeLineRow = $('<div class="row timeline"></div>').data('year',data);
 		var leftArrow = $('<div class="arrow arrow-left col-lg-1 col-md-1 col-sm-1 col-xs-1"><i class="fa fa-chevron-left" aria-hidden="true"></i></div>');
 		var rightArrow = $('<div class="arrow arrow-right col-lg-1 col-md-1 col-sm-1 col-xs-1 disabled"><i class="fa fa-chevron-right" aria-hidden="true"></i></div>');
 		timeLineRow.append(leftArrow);
 		for(var i=2;i>=0;i--){
 			var outerDiv = $('<div class="col-lg-3 col-md-3 col-sm-3 col-xs-3 year"></div>');
-			
-			//var yearRow = $('<div class="row yearRow"></div>');
-			//var yearContent = $('<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 yearContent">'+data.albums[i].year+'</div>');
 			var yearContent = $('<div class="yearContent">'+data.albums[i].year+'</div>');
-			//var selectorRow = $('<div class="row"></div>');
 			var yearSelectorClass = data.albums[i].year;
-			//var selector = $('<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 selector"></div>').addClass(yearSelectorClass);
 			var selector = $('<div class="selector"></div>');
-			
 			if(i==0)
 				{
 				  outerDiv.addClass('active');
@@ -57,13 +77,8 @@
 			else if(i==2){
 				  yearContent.addClass('currMinYear');
 			}
-			
-			//yearRow.append(yearContent);
-			//selectorRow.append(selector);
-			
 			outerDiv.append(yearContent)
 					.append(selector);
-			
 			timeLineRow.append(outerDiv);
 		}
 		timeLineRow.append(rightArrow);
@@ -74,11 +89,10 @@
 	
 	var createPictureGallery = function(year,eventData){
 		var albumContent = $('<div class="row"></div>');
-		$('.imageContainer').html('');
 		var data = eventData.pics;
 		for(var i=0;i<data.length;i++){
 			var imgsrc = data[i].thumbnail_url;
-			var outerDiv = $('<div class="col-lg-3 col-md-4 col-sm-4 col-xs-6 mainDiv picDiv"></div>')
+			var outerDiv = $('<div class="col-lg-3 col-md-3 col-sm-5 col-xs-5 mainDiv picDiv"></div>')
 							.data('full', data[i].full_url)
 							.data('index', i)
 							.data('heading', data[i].title)
@@ -87,24 +101,20 @@
 			outerDiv.append(albumImage)
 					.on('click', showImage);
 			albumContent.append(outerDiv);
-			//if(i%3==2 || (data.length-1==i))
-			  // {
 				   $('.imageContainer').append(albumContent);
-				   //albumContent = $('<div class="row"></div>');
-			   //}
 		}
-		$('.albumContainer').fadeOut('slow');
 		$('#picsHeader').text(eventData.name);
 		$('#albumSummary').text(eventData.summary);
 		$('.albumBreadcrumbs').fadeIn("slow");
 		$('#eventName').addClass('makeLink');
-		$('.imageContainer').data('pics', data).fadeIn("slow")//.show();
+		$('.imageContainer').data('pics', data).fadeIn("slow");
+		load($('.overall'),false);//.fadeIn("slow")//.show();
 	};
 	
 	var createAlbums = function(data, year){
 		var galleryContent = $('<div class="row"></div>');
-		$('.albumContainer').html('').hide();
 		$('.albumBreadcrumbs').hide();
+		$('.albumContainer').html('');
 		for(var i=0;i<data.albums.length;i++)
 			{
 			 if(data.albums[i].year == year){
@@ -113,33 +123,31 @@
 					 {
 					   var imgsrc = events[j].pics[0].thumbnail_url;
 					   var albumName = events[j].name;
-					   var outerDiv = $('<div class="col-lg-3 col-md-4 col-sm-4 col-xs-6 mainDiv"></div>').data('pics', events[j]);
+					   var outerDiv = $('<div class="col-lg-3 col-md-3 col-sm-3 col-xs-5 mainDiv"></div>').data('pics', events[j]);
 					   var albumImage = $('<img class="faceOfAlbum"></img>').attr('src',imgsrc);
 					   var innerDiv = $('<div class="divOverlay"></div>');
 					   var picDesc  = $('<div class="albumName"></div>').text(albumName);
 					   outerDiv.append(albumImage)
-					   		  // .append(innerDiv)
 					   		   .append(picDesc)
 					   		   .data('eventData', events[j])
 					   		   .on('click', function(){
+					   			   	$('.albumContainer').hide();
+					   				$('.imageContainer').html('');
+					   				load($('.overall'),true);
 					   			   	createPictureGallery(year,$(this).data('pics'));
 					   		   		});
 					   galleryContent.append(outerDiv);
-					   //if(j%3==2 || (events.length-1==j))
-						 //  {
 						   $('.albumContainer').append(galleryContent);
-						   //galleryContent = $('<div class="row"></div>');
-						   //}
-
 					 }
 			  break;	 
 			 }
 			 else
 				 continue;
 			}
-		$('.imageContainer').fadeOut();
 		$('#eventName').text("BAO Photo Gallery "+selectedYear);
 		$('.albumContainer').fadeIn();
+		load($('.overall'),false);
+
 		//console.log(galleryContent);
 	};
 	
@@ -150,6 +158,7 @@
 		var noOfYears = data.albums.length;
 		minYear = data.albums[noOfYears-1].year;
 		createTimeLine(data);
+		$('.albumContainer').html('').hide();
 		createAlbums(data, selectedYear);
 		});
 	
@@ -157,56 +166,29 @@
 					if (e.target.class != 'content' && e.target.class!= 'mainDiv' && !$('.content').find(e.target).length && !$('.mainDiv').find(e.target).length) {
 						$(".overlay, .content").hide();
 						$('body').removeClass('noScroll');
-
 				    }
 				}).on('click', '.closeContainer', function(){
 					$('.overlay, .content').hide();
+					$('.content img').attr('src','').data('currImgIndex',0);
+					$('.picHeading').text('');
+					$('.picDesc').text('');
 					$('body').removeClass('noScroll');
 
 				}).on('click', '.img-prev' ,function(){
-					var currIndex = $('.content img').data('currImgIndex');
-					var pics = $('.imageContainer').data('pics');
-					if(pics[currIndex-1]){
-						$('.content img').attr('src',pics[currIndex-1].full_url).data('currImgIndex',currIndex-1);
-					}
-					changePicNavigatorState(pics, currIndex-1);
+					picNavigator(this,'prev');
 				}).on('click', '.img-next' ,function(){
-					var currIndex = $('.content img').data('currImgIndex');
-					var pics = $('.imageContainer').data('pics');
-					if(pics[currIndex+1]){
-						$('.content img').attr('src',pics[currIndex+1].full_url).data('currImgIndex',currIndex+1);
-					}
-					changePicNavigatorState(pics, currIndex+1);
+					picNavigator(this,'next');
 				}).on('click', '.year' ,function(){
+					load($('.overall'),true);
 					$('.year').removeClass('active')
 					$(this).addClass('active');
 					selectedYear = $(this).text();
+					$('.imageContainer').hide();
 					createAlbums(picJson, selectedYear);
 				}).on('click', '.arrow-left' ,function(){
-					if(!$(this).hasClass('disabled'))
-					{
-							$('.year').hide();
-							$('.timeline .year').each(function(index){
-								var elem = $(this).find('.yearContent');
-								elem.text(parseInt(elem.text())-1);
-							});
-							$('.year').fadeIn(50);
-							changeTimelineState('left');
-							maintainYearSelection();
-
-					}
+					timeLineNavigator(this, 'prev');
 				}).on('click', '.arrow-right' ,function(){
-					if(!$(this).hasClass('disabled'))
-					{
-						$('.year').hide();
-						$('.timeline .year').each(function(index){
-							var elem = $(this).find('.yearContent');
-							elem.text(parseInt(elem.text())+1);
-						});
-						$('.year').fadeIn(50);
-						changeTimelineState('right');
-						maintainYearSelection();
-					}
+					timeLineNavigator(this, 'next');
 				}).on('click', '.makeLink', function(){
 					$('.imageContainer').fadeOut();
 					$('#eventName').removeClass('makeLink');
@@ -232,6 +214,56 @@
 		$('.arrow-left').removeClass('disabled');
 
 	}
+	var picNavigator = function(elem,direction){
+		if(!$(elem).hasClass('disabled')){
+			load($('.content'),true);
+			var currIndex = $('.content img').data('currImgIndex');
+			var pics = $('.imageContainer').data('pics');
+			var indexToChange = 0;
+			switch(direction){
+				case 'prev':
+					indexToChange = currIndex-1;
+					break;
+				case 'next':
+					indexToChange = currIndex+1;
+					break;
+				default:
+					//do nothing
+			}
+			if(pics[indexToChange]){
+				$('.content img').attr('src',pics[indexToChange].full_url).data('currImgIndex',indexToChange);
+			}
+			changePicNavigatorState(pics, indexToChange);
+			load($('.content'),false);
+		}
+	}
+	
+	var timeLineNavigator = function(elem,direction){
+		if(!$(elem).hasClass('disabled'))
+		{
+				$('.year').hide();
+				var changeYearBy = 0;
+				switch(direction){
+					case 'prev':
+						changeYearBy = -1;
+						break;
+					case 'next':
+						changeYearBy = 1;
+						break;
+					default:
+						//do nothing!
+				}
+				$('.timeline .year').each(function(index){
+					var elem = $(this).find('.yearContent');
+					elem.text(parseInt(elem.text())+(changeYearBy));
+				});
+				$('.year').fadeIn(50);
+				changeTimelineState('left');
+				maintainYearSelection();
+
+		}
+	}
+	
 	
 	var changePicNavigatorState = function(pics, currIndex){
 		if(pics[currIndex-1])
