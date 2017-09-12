d9bab992066afadeb5859e46d17a1b3dce816164
jenny <lyndle0890@gmail.com>
2017-09-11 17:03:58 -0700

    final version integration

---
+++ picGallery/final/main.js
@@ -0,0 +1,49 @@
+(function($) {
+	'use strict';
+	
+	var showGallery = function(event, val) {
+		$('.overall').load();
+		//setTimeout(function(){
+		$('.overall').album({
+			visible : false
+		});
+		$('.overall').gallery({
+			data : val.value,
+			showImage : showImage,
+			visible : true
+		});
+		$('.overall').load('destroy');//},10000);
+	}
+	var showAlbums = function(event, val) {
+		$('.overall').load();
+	//	setTimeout(function(){
+			$('.overall').gallery({
+				visible : false
+			});
+			$('.overall').album({
+				visible : true,
+				selectedYear : val.selectedYear,
+				data : val.data,
+				albumOpened : showGallery
+			});
+			$('.overall').load('destroy');
+		//},10000);
+	}
+	var showImage = function(event, data) {
+		$('.overall').imageDetail({
+			imgSrc : data.imgSrc,
+			imgHeading : data.imgHeading,
+			imgDescription : data.imgDescription,
+			pics : data.pics,
+			index : data.index,
+			visibility : true
+		});
+	}
+	$.get("https://jennylyndle.github.io/picGallery/JS/custom/pic.json",
+			function(data) {
+				$('.timeLineContainer').timeline({
+					data : data,
+					yearChange : showAlbums
+				});
+			})
+})(jQuery);
